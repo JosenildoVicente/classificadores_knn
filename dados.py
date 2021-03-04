44c3a2387951cf1b801353e66af6573fab324c8b
Josenildo <jva@cin.ufpe.br>
2021-03-04 11:50:01 -0300

    Correcao do get dados

--- dados.py
+++ dados.py
@@ -8,18 +8,13 @@ atributos = [21, 21]
 def get_dados(indice):
     data = arff.loadarff('dados/' + nomes[indice]+'.arff')
     df = pd.DataFrame(data[0])
+    df['defects'] = df['defects'].astype(str)
     x = df.iloc[:, :-1].values
     y = df.iloc[:, atributos[indice]].values
     return [x,y]
 
 
 if __name__ == '__main__':
-    # data_cm1 = arff.loadarff('dados/cm1.arff')
-    # df_cm1 = pd.DataFrame(data_cm1[0])
-
-    # data_pc1 = arff.loadarff('dados/pc1.arff')
-    # df_pc1 = pd.DataFrame(data_pc1[0])
-    [a,b] = get_dados(1)
-    
-    print(a)
-    print(b)
+    [a,b] = get_dados(0)
+    print("X: {}".format(a))
+    print("Y: {}".format(b))
