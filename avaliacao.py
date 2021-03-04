3bb1e811ef5323c87222434102f3c103b89a45ba
Josenildo <jva@cin.ufpe.br>
2021-03-04 15:18:17 -0300

    Adicao dos graficos

--- avaliacao.py
+++ avaliacao.py
@@ -1,31 +1,100 @@
-import time
-from dados import get_dados
+import knn_semPeso
+from dados import get_dados, nomes as nome_dados
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-dark-palette')
+import os
+import pandas as pd
 
 Ks = [1, 2, 3, 5, 7, 11, 13, 15]
 
 relatorios = {} #--
 
+nome_algoritmos = ["knn_sem_peso", "knn_com_peso", "knn_adaptativo"]
+treinadores = [knn_semPeso,knn_semPeso, knn_semPeso]
+
 def treinar(indice_dados, treinador, nome_algoritmo):
     x,y = get_dados(indice_dados)
 
+    relatorios[nome_algoritmo] = {}
     for k in Ks:
         treinador.K = k
+        relatorios[nome_algoritmo][k] = {}
 
-        x_treino, x_test, y_treino, y_teste = train_test_split(x, y,random_state=1)
+        x_treino, x_teste, y_treino, y_teste = train_test_split(x, y)
+        
+        relatorio = treinador.rodar(x_treino, y_treino, x_teste, y_teste)
+       
+        relatorios[nome_algoritmo][k] = relatorio
 
-        comeco = time.time()
-        treinador.treinamento(x_treino, y_treino)
-        tempo_treino = time.time() - comeco
+def plotar_graficos(indice_dados):
+    acuracias = []
+    tempos_treino = []
+    tempos_teste = []
+    
+    for i in range(len(treinadores)):
 
-        comeco = time.time()
-        relatorio = treinador.teste(x_test, y_teste)
-        tempo_teste = time.time() - comeco
+        treinar(indice_dados,treinadores[i], nome_algoritmos[i])
+        
+        acuracias.append([])
+        tempos_teste.append([])
+        tempos_treino.append([])
+        
+        for k in Ks:
+            acuracias[i].append(relatorios[nome_algoritmos[i]][k]['acuracia'])
+            tempos_treino[i].append(relatorios[nome_algoritmos[i]][k]['tempo_treino'])
+            tempos_teste[i].append(relatorios[nome_algoritmos[i]][k]['tempo_teste'])
+        
+    # folder_name = "graficos/" + nome_dados[indice_dados] + "/"
+    # if os.path.exists(folder_name) == False:
+    #         os.makedirs(folder_name, mode = 0o666)
+    
+    df_acuracias = pd.DataFrame(acuracias)
+    df_acuracias.index = nome_algoritmos[:len(treinadores)]
+    df_acuracias.columns = Ks
+    df_acuracias = df_acuracias.T
 
-        relatorio['tempo_treinamento'] = int(tempo_treino * 1000)
-        relatorio['tempo_teste'] = int(tempo_teste * 1000)
+    df_tempos_treinamento = pd.DataFrame(tempos_treino)
+    df_tempos_treinamento.index = nome_algoritmos[:len(treinadores)]
+    df_tempos_treinamento.columns = Ks
+    df_tempos_treinamento = df_tempos_treinamento.T
+    
+    df_tempos_teste = pd.DataFrame(tempos_teste)
+    df_tempos_teste.index = nome_algoritmos[:len(treinadores)]
+    df_tempos_teste.columns = Ks
+    df_tempos_teste = df_tempos_teste.T
+    
+    df_acuracias.plot()
+    plt.title("Acurácias dos algoritmos em relação aos Ks")
+    plt.xlabel("Valor do K")
+    plt.ylabel("Acurácia")
+    # plt.savefig(folder_name + 'acuracias.png', format='png')
+    plt.show()
 
-        relatorios[nome_algoritmo][k].append(relatorio)
+    df_tempos_treinamento.plot()
+    plt.title("Tempo de treinamento dos algoritmos em relação aos Ks")
+    plt.xlabel("Valor do K")
+    plt.ylabel("Tempo de treinamento (em ms)")
+    # plt.savefig(folder_name + 'tempos_treino.png', format='png')
+    plt.show()
 
-    return None
+    df_tempos_teste.plot()
+    plt.title("Tempo de teste dos algoritmos em relação aos Ks")
+    plt.xlabel("Valor do K")
+    plt.ylabel("Tempo de teste (em ms")
+    # plt.savefig(folder_name + 'tempos_teste.png', format='png')
+    plt.show()
 
+
+
+if __name__ == '__main__':
+    plotar_graficos(0)
+    plotar_graficos(1)
+    # treinar(0, knn_semPeso,nome_algoritmos[0])
+    # print(relatorios['knn_sem_peso'])
+    # print(relatorios['knn_sem_peso'][7])
+    # treinar(1, knn_semPeso,"knn_sem_peso")
+
+    # relatorios
+
+    
--- knn_semPeso.py
+++ knn_semPeso.py
@@ -1,17 +1,44 @@
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+import time
 
 K = 5
 
-modelo = modelo = KNeighborsClassifier(n_neighbors=K, weights='uniform')
+modelo = KNeighborsClassifier(n_neighbors=K, weights='uniform')
 
-def treinamento(x_treinamento, y_treinamento):
+def rodar(x_treinamento, y_treinamento, x_teste, y_teste):
+
+    tempo_inicio = time.time()
     modelo = KNeighborsClassifier(n_neighbors=K, weights='uniform')
     modelo.fit(x_treinamento, y_treinamento)
-    # return modelo
+    tempo_treino = time.time() - tempo_inicio
 
-def teste(x_teste, y_teste):
+    tempo_inicio = time.time()
     y_modelo = modelo.predict(x_teste)
-    report = classification_report(y_teste,y_modelo, output_dict=True)
-    return report
+    tempo_teste = time.time() - tempo_inicio
+
+    acuracia = accuracy_score(y_modelo, y_teste)
+    
+    resultado = {}
+    resultado['tempo_treino'] = int(tempo_treino * 1000)
+    resultado['tempo_teste'] = int(tempo_teste * 1000)
+    resultado['acuracia'] = acuracia
+    
+    # print(y_teste == y_modelo)
+
+    # print(y_modelo)
+    # report = classification_report(y_teste, y_modelo, output_dict=True)
+    return resultado
+    # return report
+    # return teste(x_test,y_test)
+
+# def treinamento(x_treinamento, y_treinamento):
+#     modelo = KNeighborsClassifier(n_neighbors=K, weights='uniform')
+#     modelo.fit(x_treinamento, y_treinamento)
+#     y_modelo = modelo.predict(x_treinamento)
+
+# def teste(x_teste, y_teste):
+#     y_modelo = modelo.predict(x_teste)
+#     report = classification_report(y_teste,y_modelo, output_dict=True)
+#     return report
 
